8f690b31e54b4d92b342a8c43b337902e5ca489d
huynq02 <huynq02@ominext.com>
2024-06-27 13:40:16 +0700

    remove file unnecessary

--- lesson_8/cypress/support/e2e.js
+++ lesson_8/cypress/support/e2e.js
@@ -20,8 +20,3 @@ import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // Returning false here prevents Cypress from
-    // failing the test
-    return false;
-});
